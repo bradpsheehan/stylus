f63427554f1994c544f88990341e4757af96d964
Bradley Sheehan <bradpsheehan@gmail.com>
2018-05-06 20:03:04 -0400

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+raw_artist_data = [{ name: 'Kanye West' }, { name: 'Dave Matthews Band' }]
 
-  artists = Artist.create([{ name: 'Kanye West' }, { name: 'Dave Matthews Band' }])
-  AudioRecord.create(title: 'Graduation', artist: artists.first, released_on: Date.today, record_condition: 'Great')
-  AudioRecord.create(title: 'Late Registration', artist: artists.first, released_on: 1.year.ago, record_condition: 'Great')
-  AudioRecord.create(title: 'Freshmen Adjustment', artist: artists.first, released_on: Date.today, record_condition: 'Great')
+artists = raw_artist_data.map do |artist|
+  Artist.where(name: artist[:name]).first_or_create
+end
 
-  AudioRecord.create(title: 'Crash', artist: artists.last, released_on: 2.years.ago, record_condition: 'Great')
-  AudioRecord.create(title: 'Under Table', artist: artists.last, released_on: 2.years.ago, record_condition: 'Great')
-  AudioRecord.create(title: 'Another Record', artist: artists.last, released_on: Date.today, record_condition: 'Great')
+AudioRecord.where(title: 'Late Registration').first_or_create(artist: artists.first, released_on: 1.year.ago, record_condition: 'Poor')
+AudioRecord.where(title: 'Graduation').first_or_create(artist: artists.first, released_on: Date.today, record_condition: 'Great')
+AudioRecord.where(title: 'Freshmen Adjustment').first_or_create(artist: artists.first, released_on: 5.days.ago, record_condition: 'Amazing')
+
+AudioRecord.where(title: 'Red Rocks').first_or_create(artist: artists.last, released_on: 2.years.ago, record_condition: 'Great')
+AudioRecord.where(title: 'Crash').first_or_create(artist: artists.last, released_on: 2.years.ago, record_condition: 'Acceptable')
+AudioRecord.where(title: 'Under Table').first_or_create(artist: artists.last, released_on: 2.years.ago, record_condition: 'Poor')
+AudioRecord.where(title: 'Everyday').first_or_create(artist: artists.last, released_on: Date.today, record_condition: 'Great')
+AudioRecord.where(title: 'Stand Up').first_or_create(artist: artists.last, released_on: 2.days.ago, record_condition: 'Perfect')
+AudioRecord.where(title: 'Busted Stuff').first_or_create(artist: artists.last, released_on: 4.years.ago, record_condition: 'Great')
 
 
 
